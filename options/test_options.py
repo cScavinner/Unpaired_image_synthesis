481b5264349a61c0ec5efd519b9af3540813aa86
Claire Scavinner--Dorval <73347729+cScavinner@users.noreply.github.com>
2024-04-19 16:47:45 +0200

    Update test_options.py

--- options/test_options.py
+++ options/test_options.py
@@ -13,15 +13,14 @@ class TestOptions(base_options.BaseOptions):
         parser.add_argument('--patch_overlap', help='Patch overlap', type=int, required=False, default = (0,0,0))
         parser.add_argument('-b', '--batch_size', help='Batch size', type=int, required=False, default = 1)
         parser.add_argument('-g', '--ground_truth', help='Ground truth for metric computation', type=str, required=False)
-        parser.add_argument('-D', '--dataset', help='Name of the dataset used', type=str, required=False, default='hcp')
-        parser.add_argument('--gpu', help='GPU to use (If None, goes on CPU)', type=int,required=False, default=0)
-        parser.add_argument('--seg', help='Use static segmentations as prior', type=str, required=False)
+        parser.add_argument('-D', '--dataset', help='Name of the dataset used', type=str, required=False, default='equinus')
+        parser.add_argument('--gpu', help='GPU to use (If -1, goes on CPU)', type=int,required=False, default=0)
 
         subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
 
-        DRIT_parser = subparsers.add_parser("DRIT", help="DRIT mode parser")
-        DRIT_parser.add_argument('--mode', help='Mode to use (for DRIT or CycleGAN): reconstruction or degradation', type=str,required=False, default='reconstruction')
-        DRIT_parser.add_argument('--latents', help='Get latent variables', action='store_true')
+        mDRIT_parser = subparsers.add_parser("mDRIT", help="mDRIT++ mode parser")
+        mDRIT_parser.add_argument('--mode', help='Mode to use: reconstruction or degradation', type=str,required=False, default='reconstruction')
+        mDRIT_parser.add_argument('--latents', help='Get latent variables', action='store_true')
         
         self.isTrain= False
         return parser
