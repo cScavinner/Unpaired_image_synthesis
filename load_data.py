d516327b1d8ab39559b160194c01130fcd7a2424
Claire Scavinner--Dorval <73347729+cScavinner@users.noreply.github.com>
2024-05-01 14:20:47 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,1 +1,9 @@
+import glob
+import os
+import torchio as tio
+import sys
 
+def load_data(data, segmentation, dynamic_path = None, static_path = None, seg_path=None, footmask_path=None):
+    subjects=[]
+    
+    return(subjects, check_subjects)
